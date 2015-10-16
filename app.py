edb31d844ab2d92527966dd674683af318d1a409
atrp-- <dfjkhgshakazulu3091@gmail.com>
2015-10-16 11:24:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ def home():
         session['logged']=False
     if request.method=="POST":
         post_data.addPost(request.form['story'],request.form['title'],session['username'])
+    print request.environ['REMOTE_ADDR']
     return render_template('home.html',s=session)
 
 @app.route('/login',methods=["GET","POST"])
